f16871b0d9a3f2c0dfba40c2a188a4ebac52313a
tmomonet <thollins@cnm.edu>
2025-06-16 09:41:07 -0600

    TODOs in gradle.properties, settings.gradle. Changed settings.gradle to Maven convention, change to server main application and tests

--- server/src/test/java/edu/cnm/deepdive/capstone/CrossfyreApplicationTests.java
+++ server/src/test/java/edu/cnm/deepdive/capstone/CrossfyreApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CrossfyreTests {
+class CrossfyreApplicationTests {
 
 	@Test
 	void contextLoads() {
