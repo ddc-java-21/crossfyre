cc991430614d1a933112fde613b0857e8dc3efe0
tmomonet <thollins@cnm.edu>
2025-07-07 12:19:13 -0600

    reversing through to get build back working

--- app/src/main/java/edu/cnm/deepdive/crossfyre/package-info.java
+++ app/src/main/java/edu/cnm/deepdive/crossfyre/package-info.java
@@ -3,4 +3,3 @@
  * those imported from external libraries) are contained in this package and its sub-packages.
  */
 package edu.cnm.deepdive.crossfyre;
-// TODO: Rename this package to match basePackageName in gradle.properties.
