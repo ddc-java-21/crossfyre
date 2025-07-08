3aa5a970d055952f2108a255f3aabbca4fade594
tanickell <tnickell1@cnm.edu>
2025-07-07 19:38:44 -0600

    Updated ddl-server.sql to list DDL statements for updated entity classes.

--- docs/sql/ddl-server.sql
+++ docs/sql/ddl-server.sql
@@ -1,83 +1,63 @@
-create sequence game_seq start with 1 increment by 50;
-create sequence solution_puzzle_seq start with 1 increment by 50;
-create sequence solution_word_seq start with 1 increment by 50;
-create sequence user_profile_seq start with 1 increment by 50;
+create sequence guess_seq start with 1 increment by 50;
+create sequence puzzle_seq start with 1 increment by 50;
+create sequence puzzle_word_seq start with 1 increment by 50;
 create sequence user_puzzle_seq start with 1 increment by 50;
-create sequence user_word_seq start with 1 increment by 50;
-create table game
+create sequence user_seq start with 1 increment by 50;
+create table guess
 (
-    completed      timestamp(6) with time zone not null,
-    game_id        bigint                      not null,
-    player_id      bigint                      not null unique,
-    posted         timestamp(6) with time zone not null,
-    user_puzzle_id bigint                      not null unique,
-    external_key   uuid                        not null unique,
-    primary key (game_id)
+    guess_char     char(1) not null,
+    guess_column   integer not null,
+    guess_row      integer not null,
+    guess_id       bigint  not null,
+    user_puzzle_id bigint  not null,
+    primary key (guess_id)
 );
-create table solution_puzzle
+create table puzzle
 (
-    size               integer                     not null,
-    created            timestamp(6) with time zone not null,
-    solution_puzzle_id bigint                      not null,
-    external_key       uuid                        not null unique,
-    title              varchar(30)                 not null unique,
-    board              varchar(255)                not null,
-    primary key (solution_puzzle_id)
+    size         integer                     not null,
+    created      timestamp(6) with time zone not null,
+    puzzle_id    bigint                      not null,
+    title        varchar(15)                 not null unique,
+    external_key uuid                        not null unique,
+    board        varchar(255)                not null,
+    primary key (puzzle_id)
 );
-create table solution_word
+create table puzzle_word
 (
-    word_column        integer                     not null,
-    word_row           integer                     not null,
-    posted             timestamp(6) with time zone not null,
-    solution_puzzle_id bigint                      not null,
-    solution_word_id   bigint                      not null,
-    external_key       uuid                        not null unique,
-    clue               varchar(255)                not null unique,
-    word_direction     varchar(255)                not null,
-    word_name          varchar(255)                not null,
-    primary key (solution_word_id)
+    word_column    integer      not null,
+    word_row       integer      not null,
+    puzzle_id      bigint       not null,
+    puzzle_word_id bigint       not null,
+    clue           varchar(255) not null unique,
+    word_direction varchar(255) not null,
+    word_name      varchar(255) not null unique,
+    primary key (puzzle_word_id)
 );
-create table user_profile
+create table user
 (
-    created         timestamp(6) with time zone not null,
-    user_profile_id bigint                      not null,
-    external_key    uuid                        not null unique,
-    display_name    varchar(30)                 not null unique,
-    oauth_key       varchar(30)                 not null unique,
-    avatar          varchar(255),
-    primary key (user_profile_id)
+    created      timestamp(6) with time zone not null,
+    user_id      bigint                      not null,
+    external_key uuid                        not null unique,
+    display_name varchar(20)                 not null,
+    oauth_key    varchar(30)                 not null unique,
+    avatar       varchar(255),
+    primary key (user_id)
 );
 create table user_puzzle
 (
-    size               integer                     not null,
-    created            timestamp(6) with time zone not null,
-    solution_puzzle_id bigint                      not null,
-    user_puzzle_id     bigint                      not null,
-    external_key       uuid                        not null unique,
-    title              varchar(30)                 not null unique,
-    board              varchar(255)                not null,
+    created        timestamp(6) with time zone not null,
+    puzzle_id      bigint                      not null,
+    solved         timestamp(6) with time zone not null,
+    user_id        bigint                      not null,
+    user_puzzle_id bigint                      not null,
+    external_key   uuid                        not null unique,
     primary key (user_puzzle_id)
 );
-create table user_word
-(
-    word_column    integer                     not null,
-    word_row       integer                     not null,
-    posted         timestamp(6) with time zone not null,
-    user_puzzle_id bigint                      not null,
-    user_word_id   bigint                      not null,
-    external_key   uuid                        not null unique,
-    clue           varchar(255)                not null unique,
-    word_direction varchar(255)                not null,
-    word_name      varchar(255)                not null,
-    primary key (user_word_id)
-);
-alter table if exists game
-    add constraint FKe2gg67v12imv2fr2nx0af0usn foreign key (player_id) references user_profile;
-alter table if exists game
-    add constraint FKggbqyowbcjj60mhpp2td22242 foreign key (user_puzzle_id) references user_puzzle;
-alter table if exists solution_word
-    add constraint FKp8hcr0hrq4fwjy21h4shggx39 foreign key (solution_puzzle_id) references solution_puzzle;
+alter table if exists guess
+    add constraint FKmayfx7rfqnos60y7jtvgx0u3s foreign key (user_puzzle_id) references user_puzzle;
+alter table if exists puzzle_word
+    add constraint FK6qqef7f12yg9ksowywhd4qou0 foreign key (puzzle_id) references puzzle;
 alter table if exists user_puzzle
-    add constraint FKnt5dqf3m5a84px3lidh3mm9gl foreign key (solution_puzzle_id) references solution_puzzle;
-alter table if exists user_word
-    add constraint FK8mf6onh6mbt9dx24fwopoj2d9 foreign key (user_puzzle_id) references user_puzzle;
+    add constraint FK8o0f33pxc4w5arng3v9ld02mn foreign key (puzzle_id) references puzzle;
+alter table if exists user_puzzle
+    add constraint FKi9qppcfilrktf0oy1qmpuxbtp foreign key (user_id) references user;
